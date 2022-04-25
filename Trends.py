ba88addc8382b9d97710ebbb6c73f5259a86aa7d
Vivek Tiwari <viv.tiwari13@gmail.com>
2022-04-25 23:58:20 +0530

    Add files via upload

--- Trends.py
+++ Trends.py
@@ -1,40 +1,59 @@
 #get share price of Tesla and Twitter of last 6 months and create a streamlit app
 
-import yfinance as yf
+# import yfinance as yf
 import streamlit as st
 import pandas as pd
 import numpy as np
 import plotly.graph_objects as go
+import pandas as pd
+import matplotlib.pyplot as plt
+import pandas_datareader as web
+import mplfinance as mpf
+import datetime as dt
+
 
 st.title("Stock Price Analysis")
 
 #Multiselect box to get the charts and info of the stocks
 tickerSymbol = st.multiselect('Select the ticker symbols of the stocks',('TSLA','TWTR','GOOGL','AMZN','FB','AAPL'))
 
-#get the closing price of the stocks
-def get_close_stock_price(symbols, start_date, end_date):
+#get the closing price of the stocks using mplfinance
+def get_close_stock_price_mplfinance(symbols, start_date, end_date):
     df = pd.DataFrame()
     for symbol in symbols:
-        df[symbol] = yf.download(symbol, start_date, end_date)['Adj Close']
+        df[symbol] = web.DataReader(symbol, 'yahoo', start_date, end_date)['Adj Close']
     return df
+# def get_close_stock_price(symbols, start_date, end_date):
+#     df = pd.DataFrame()
+#     for symbol in symbols:
+#         df[symbol] = yf.download(symbol, start_date, end_date)['Adj Close']
+#     return df
 
-#get the volume of the stocks
-def get_volume_stock_price(symbols, start_date, end_date):
+#get the volume of the stocks using mplfinance
+def get_volume_stock_price_mplfinance(symbols, start_date, end_date):
     df = pd.DataFrame()
     for symbol in symbols:
-        df[symbol] = yf.download(symbol, start_date, end_date)['Volume']
+        df[symbol] = web.DataReader(symbol, 'yahoo', start_date, end_date)['Volume']
         df[symbol] = df[symbol].astype(int)
     return df
+# def get_volume_stock_price(symbols, start_date, end_date):
+#     df = pd.DataFrame()
+#     for symbol in symbols:
+#         df[symbol] = yf.download(symbol, start_date, end_date)['Volume']
+#         df[symbol] = df[symbol].astype(int)
+#     return df
 
 #get the closing price of the stocks end date to be today
 start_date = st.sidebar.date_input('Start Date', value=pd.Timestamp('2021-04-01'))
 end_date = st.sidebar.date_input('End Date', value=pd.Timestamp('2022-04-01'))
 
 #based on dates selected and stocks selected get the line charts for the shares
-df = get_close_stock_price(tickerSymbol, start_date, end_date)
+df = get_close_stock_price_mplfinance(tickerSymbol, start_date, end_date)
+# df = get_close_stock_price(tickerSymbol, start_date, end_date)
 
-#get the volume of the stocks
-df_volume = get_volume_stock_price(tickerSymbol, start_date, end_date)
+#get the volume of the stocks end date to be today
+df_volume = get_volume_stock_price_mplfinance(tickerSymbol, start_date, end_date)
+# df_volume = get_volume_stock_price(tickerSymbol, start_date, end_date)
 
 #line chart for the closing price of the stocks
 st.header('Stock Closing Price' + ' (' + start_date.strftime('%Y-%m-%d') + ' to ' + end_date.strftime('%Y-%m-%d') + ')')
